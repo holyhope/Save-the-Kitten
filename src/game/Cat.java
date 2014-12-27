bef718483667acac1fe4d6e0acdf6d66d83e7bee
holyhope <pierre.peronnet@gmail.com>
2014-12-27 11:49:05 +0100

    Documentation. Suppression de paint() pour la remplacer par draw()

--- src/game/Cat.java
+++ src/game/Cat.java
@@ -1,6 +1,5 @@
 package game;
 
-import java.awt.Graphics;
 import java.awt.Graphics2D;
 
 import org.jbox2d.common.Vec2;
@@ -9,10 +8,23 @@ import org.jbox2d.dynamics.BodyDef;
 import org.jbox2d.dynamics.World;
 
 public final class Cat extends Bullet {
-	public static Cat create(World world, Vec2 vec2, Vec2 velocity,
+	/**
+	 * Create a cat
+	 * 
+	 * @param world
+	 *            of the cat
+	 * @param position
+	 *            of the cat
+	 * @param velocity
+	 *            of the cat
+	 * @param angularVelocity
+	 *            of the cat. Make it rolling.
+	 * @return
+	 */
+	public static Cat create(World world, Vec2 position, Vec2 velocity,
 			float angularVelocity) {
 		Body body;
-		BodyDef bodyDef = getBodyDef(vec2, velocity, angularVelocity);
+		BodyDef bodyDef = getBodyDef(position, velocity, angularVelocity);
 		do {
 			body = world.createBody(bodyDef);
 		} while (body == null);
@@ -20,31 +32,50 @@ public final class Cat extends Bullet {
 
 	}
 
+	/**
+	 * Create a cat
+	 * 
+	 * @param world
+	 *            of the cat
+	 * @param position
+	 *            of the cat
+	 * @param velocity
+	 *            of the cat
+	 * @return
+	 */
 	public static Cat create(World world, Vec2 position, Vec2 velocity) {
 		return create(world, position, velocity, 0);
 	}
 
-	public Cat(Body body) {
+	private Cat(Body body) {
 		super(body, getFixtureDef());
 	}
 
-	public void paint(Graphics g) {
-		Vec2 position = getPosition();
-		g.fillRect(Math.round(position.x), Math.round(position.y), 10, 10);
-	}
-
+	/**
+	 * Called when cat contact another body
+	 */
 	@Override
 	public void beginContact(Body body) {
 		super.beginContact(body);
 		stop();
 	}
 
+	/**
+	 * Called on contact end
+	 */
 	@Override
 	public void endContact(Body body) {
 	}
 
+	/**
+	 * Paint cat on graphics
+	 * 
+	 * @param graphics
+	 */
 	@Override
-	public void draw(Graphics2D g) {
-
+	public void draw(Graphics2D graphics) {
+		Vec2 position = getPosition();
+		graphics.fillRect(Math.round(position.x), Math.round(position.y), 10,
+				10);
 	}
 }
